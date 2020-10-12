80039af9ef52904aab783efc6784245c70dd34e4
Marco Rezkallah <mmsrezkallah@gmail.com>
2020-10-13 01:11:22 +0200

    redirect to the same page

--- controllers/dashboard/dashboard.js
+++ controllers/dashboard/dashboard.js
@@ -8,7 +8,7 @@ module.exports.getDashboard = (req, res, next) => {
     console.log('hi');
     res.render('admin/dashboard.ejs', {
       currentPage: 'dashboard',
-      role: req.session.role,
+      role: req.session.role
     });
   } else {
     res.send('You are not authorized to view the dashboard');
@@ -17,10 +17,10 @@ module.exports.getDashboard = (req, res, next) => {
 
 module.exports.getManageCashier = (req, res, next) => {
   if (validateAdmin(req)) {
-    User.findAll().then((result) => {
+    User.findAll().then(result => {
       res.render('admin/admin.ejs', {
         users: result,
-        role: req.session.role,
+        role: req.session.role
       });
     });
   } else {
@@ -35,8 +35,10 @@ module.exports.postAddUser = async (req, res, next) => {
       username: req.body.username,
       password: password,
       role: req.body.admin == 'on' ? 'admin' : 'cashier',
-      name: req.body.name,
-    }).then(res.send());
+      name: req.body.name
+    }).then(() => {
+      res.redirect('/admin/manage-cashier');
+    });
   } else {
     res.send('You cant add cashiers');
   }
@@ -44,13 +46,14 @@ module.exports.postAddUser = async (req, res, next) => {
 
 module.exports.postEditUser = async (req, res, next) => {
   if (validateAdmin(req)) {
-    User.findByPk(req.body.username).then(async (result) => {
+    User.findByPk(req.body.username).then(async result => {
       result.name = req.body.name.toString();
       if (req.body.password) {
         result.password = await bcrypt.hash(req.body.password, salt);
       }
       result.role = req.body.admin ? 'admin' : 'cashier';
       result.save();
+      res.redirect('/admin/manage-cashier');
     });
   }
 };
